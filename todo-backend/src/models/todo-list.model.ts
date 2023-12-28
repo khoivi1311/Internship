c437d35e772c5b67c9e5e7796a30dacc4db548dd
khoivi1311 <vi.tramkhoi@gmail.com>
2023-12-28 12:58:29 +0700

    Add src code BE

---
+++ todo-backend/src/models/todo-list.model.ts
@@ -0,0 +1,32 @@
+import {Entity, hasMany, model, property} from '@loopback/repository';
+import {Todo, TodoWithRelations} from './todo.model';
+
+@model()
+export class TodoList extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @hasMany(() => Todo)
+  todos: Todo[];
+
+  constructor(data?: Partial<TodoList>) {
+    super(data);
+  }
+}
+
+export interface TodoListRelations {
+  // describe navigational properties here
+  todos?: TodoWithRelations[];
+}
+
+export type TodoListWithRelations = TodoList & TodoListRelations;
